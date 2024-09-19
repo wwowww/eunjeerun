17108b07c0896abb23b92e5f593ed681d1b9a2cf
wwowww <105021733+wwowww@users.noreply.github.com>
2024-09-19 18:07:11 +0900

    feat. slide 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import { useState } from 'react';
-import './App.css'
+import Loading from './components/templates/Loading/Loading';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import Home from './components/templates/Home/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-
+      <Loading />
+      <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+        <SwiperSlide>
+          <Home />
+        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+      </Swiper>
     </div>
   )
 }
