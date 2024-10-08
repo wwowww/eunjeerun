a96b393eb908b746d888ac28e474c43898201c1c
wwowww <105021733+wwowww@users.noreply.github.com>
2024-10-08 19:05:56 +0900

    feat. slide, pagination 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
+import SlidePage from './components/organisms/SlidePage/SlidePage';
 import Loading from './components/templates/Loading/Loading';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import { Pagination } from 'swiper/modules';
-import Home from './components/templates/Home/Home';
 
 function App() {
   return (
-    <div>
-      <Loading />
-      <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
-        <SwiperSlide>
-          <Home />
-        </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-      </Swiper>
-    </div>
+    <>
+      {/* <Loading /> */}
+      <SlidePage /> 
+    </>
   )
 }
 
