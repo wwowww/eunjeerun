d441c6f49ad6dc89f11adf86ff34efff6e7a91ea
wwowww <105021733+wwowww@users.noreply.github.com>
2024-10-10 01:41:31 +0900

    design. gradient 추가

--- src/components/molecules/Background/Background.tsx
+++ src/components/molecules/Background/Background.tsx
@@ -4,12 +4,13 @@ import style from "./Background.module.scss";
 type Props = {
   children?: ReactNode;
   hasStartLine?: boolean;
+  hasGradient?: boolean;
 }
 
-const Background = ({ children, hasStartLine=false }: Props) => {
+const Background = ({ children, hasStartLine=false, hasGradient }: Props) => {
   return (
     <div className={style.wrap}>
-      <div className={style.homeSlide}>
+      <div className={`${style.homeSlide} ${hasGradient ? style.gradient : ""}`}>
         {children}
       </div>
       <div className={style.trackWrap}>
--- src/components/templates/Home/Home.tsx
+++ src/components/templates/Home/Home.tsx
@@ -3,7 +3,7 @@ import style from "./Home.module.scss";
 
 const Home = () => {
   return (
-    <Background hasStartLine>
+    <Background hasStartLine hasGradient>
       <ul className={style.itemWrap}>
         <li className={style.sun}></li>
         <li className={style.cloud_1}></li>
