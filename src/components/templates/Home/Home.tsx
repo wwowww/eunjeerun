56c902996f8dde2412b8a1bf125e7f4f73156b4a
wwowww <105021733+wwowww@users.noreply.github.com>
2024-10-10 01:05:11 +0900

    design. home page 추가

--- src/components/templates/Home/Home.tsx
+++ src/components/templates/Home/Home.tsx
@@ -1,10 +1,19 @@
+import Background from '../../molecules/Background/Background';
 import style from "./Home.module.scss";
 
 const Home = () => {
   return (
-    <div className={style.wrap}>
-      
-    </div>
+    <Background hasStartLine>
+      <ul className={style.itemWrap}>
+        <li className={style.sun}></li>
+        <li className={style.cloud_1}></li>
+        <li className={style.cloud_2}></li>
+        <li className={style.cloud_3}></li>
+        <li className={style.cloud_4}></li>
+        <li className={style.cloud_5}></li>
+        <li className={style.cloud_6}></li>  
+      </ul>
+    </Background>
   )
 }
 
