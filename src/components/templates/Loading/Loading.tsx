fbd634a0521ada4d4711dce10dc94418c73f8e7e
wwowww <105021733+wwowww@users.noreply.github.com>
2024-10-10 21:39:29 +0900

    feat. 첫 로딩 화면 수정

--- src/components/templates/Loading/Loading.tsx
+++ src/components/templates/Loading/Loading.tsx
@@ -1,20 +1,48 @@
-import { useState } from "react";
-import Runner from "../../atoms/Runner/Runner";
+import { useEffect, useState } from "react";
+import { range } from 'lodash';
 import TypingText from "../../atoms/TypingText/TypingText";
 import style from "./Loading.module.scss";
 
 const Loading = () => {
+  const totalBars = 25;
+  const [activeBars, setActiveBars] = useState<number>(0); 
   const [isShow, setIsShow] = useState<boolean>(true);
 
   setTimeout(() => {
     setIsShow(false)
   }, 5000);
 
+  useEffect(() => {
+    if (activeBars < totalBars) {
+      const timer = setTimeout(() => {
+        setActiveBars((prev) => prev + 1);
+      }, 150);
+      return () => clearTimeout(timer);
+    }
+  }, [activeBars]);
+
   return (
     isShow && (
       <div className={style.wrap}>
-        <Runner />
-        <TypingText text="Please wait for a moment. Loading ..." frame={100} />
+        <div className={style.window}>
+          <div className={style.header}>
+            <label>Eunjee Run Update</label>
+            <ul className={style.button}>
+            {range(0, 3)?.map((i: number) => (
+              <li key={i + "list key"}></li>
+            ))}
+            </ul>
+          </div>
+          <div className={style.contents}>
+            <img src="/src/assets/images/window-loading.gif" alt="" />
+            <TypingText text="Please wait for a moment..." frame={100} />
+            <ul className={style.installBar}>
+              {range(0, totalBars)?.map((i: number) => (
+                <li key={i + "bar key"} className={i < activeBars ? style.active : ''}></li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     )
   )
