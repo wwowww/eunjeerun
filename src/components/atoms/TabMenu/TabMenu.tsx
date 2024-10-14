afbd9a9a3d0d4853ff5cd4b87d0d2e1a90de0c33
wwowww <105021733+wwowww@users.noreply.github.com>
2024-10-14 22:25:13 +0900

    feat. tabmenu 컴포넌트 추가

---
+++ src/components/atoms/TabMenu/TabMenu.tsx
@@ -0,0 +1,36 @@
+import { useState, ReactNode } from "react";
+import style from "./TabMenu.module.scss";
+
+type TabMenuProps = {
+  tabs: string[];
+  contents: ReactNode[];
+  initialActiveTab?: string;
+  
+}
+
+const TabMenu = ({ tabs, contents, initialActiveTab }: TabMenuProps) => {
+  const [activeTab, setActiveTab] = useState<string>(initialActiveTab || tabs[0]);
+
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <div className={style.wrap}>
+      {tabs.map((tab, index) => (
+        <button
+          key={index}
+          onClick={() => handleTabClick(tab)}
+          className={activeTab === tab ? style.active : ""}
+        >
+          {tab}
+        </button>
+      ))}
+      <div className={style.contents}>
+        {contents[tabs.indexOf(activeTab)]}
+      </div>   
+    </div>
+  )
+}
+
+export default TabMenu;
