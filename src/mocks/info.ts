dd5514e21d1563cbca747537d36a25c54cf67380
wwowww <105021733+wwowww@users.noreply.github.com>
2024-10-16 01:11:37 +0900

    docs. mock data 추가

---
+++ src/mocks/info.ts
@@ -0,0 +1,56 @@
+export const imageSlideInfo = [
+  {
+    image: 'todos.png',
+    link: 'https://todo-app-chi-beryl-53.vercel.app/',
+  },
+  {
+    image: 'vscode.png',
+    link: 'https://portfolio-vscode-nu.vercel.app/',
+  },
+  {
+    image: 'wemade.png',
+    link: 'https://portfolio-vscode-nu.vercel.app/',
+  }
+];
+
+export const textSlideInfo = [
+  {
+    title: "AI Speech Todo App",
+    contents: [
+      {
+        subTitle: "설명",
+        description: "AI Speech Todo App은 음성 인식 기능을 갖춘 할 일 관리 애플리케이션입니다."
+      },
+      {
+        subTitle: "사용 Skills",
+        description: "React, Node.js, MongoDB"
+      },
+      {
+        subTitle: "Link",
+        description: "https://todo-app-chi-beryl-53.vercel.app/"
+      }
+    ]
+  },
+  {
+    title: "VS Code Style Portfolio",
+    contents: [
+      {
+        subTitle: "설명",
+        description: "VS Code 스타일의 포트폴리오 웹사이트입니다."
+      }
+    ]
+  },
+  {
+    title: "Wemade 경력기술서",
+    contents: [
+      {
+        subTitle: "설명",
+        description: "Wemade에 재직하며 진행한 프로젝트 기반 경력 기술서입니다."
+      },
+      {
+        subTitle: "링크",
+        description: "클릭 시, notion으로 이동합니다."
+      }
+    ]
+  }
+]
