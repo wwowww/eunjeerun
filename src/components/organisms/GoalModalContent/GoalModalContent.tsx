599318d1560d83730dd818c40749de302fc7cc80
wwowww <105021733+wwowww@users.noreply.github.com>
2024-10-16 01:13:19 +0900

    feat. goal modal content, animation 추가

---
+++ src/components/organisms/GoalModalContent/GoalModalContent.tsx
@@ -0,0 +1,34 @@
+import OptionBar from "../../atoms/optionBar/optionBar";
+import style from "./GoalModalContent.module.scss";
+
+const GoalModalContent = () => {
+  const option = ["Go", "Run", "Challenge"];
+  
+  return (
+    <div className={style.wrap}>
+      <OptionBar options={option} />
+      <div className={style.content}>
+        <p>'꿈을 향해 끝없이 달리자!'라는 아이디어에서 시작된 은지런 프로젝트!</p>
+        <p>페이지를 자세히 보면 출발선은 있지만 결승선은 없습니다.</p>
+        <p>앞으로도 페이지 속 캐릭터처럼 꿈을 향해 끝없이 달리는 제가 되겠습니다!!</p>
+        <p>감사합니다!</p>
+        <div className={style.imageWrap}>
+          <img src="/src/assets/images/run/eunjee-run.gif" alt="" />
+          <div className={style.portal}>
+            <iframe src="https://giphy.com/embed/CTJfRJVWhE2ji4RAKR" width="352" height="480" frameBorder="0" allowFullScreen></iframe>
+          </div>
+        </div>
+      </div>
+      <ul className={style.social}>
+        <li>
+          <a href="https://github.com/wwowww" target="_blank">Github</a>
+        </li>
+        <li>
+          <a href="https://velog.io/@wwowww" target="_blank">Blog</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default GoalModalContent;
