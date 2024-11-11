aee34d947b5e8481dc68b4e1fa5747d059cd6877
wwowww <105021733+wwowww@users.noreply.github.com>
2024-11-11 21:52:04 +0900

    style. 포트폴리오 내용 수정

--- src/mocks/info.ts
+++ src/mocks/info.ts
@@ -1,108 +1,237 @@
 export const imageSlideInfo = [
   {
-    image: 'todos.png',
-    link: 'https://todo-app-chi-beryl-53.vercel.app/',
+    image: 'wepublic.webp',
+    link: 'https://kwoneunjee.notion.site/Wepublic-136b6a7ed2da8097adbccae576d50330?pvs=4',
   },
   {
-    image: 'vscode.png',
-    link: 'https://portfolio-vscode-nu.vercel.app/',
+    image: 'nile.webp',
+    link: 'https://kwoneunjee.notion.site/NFT-Nile-137b6a7ed2da80cf8b5eec7764a0777b?pvs=4',
   },
   {
-    image: 'wemade.png',
-    link: 'https://www.notion.so/kwoneunjee/121b6a7ed2da8067a5cadff36d7d1f79?pvs=4#127b6a7ed2da80399aa2e72003f633e9',
+    image: 'xdraco.webp',
+    link: 'https://kwoneunjee.notion.site/NFT-Xdraco-NFT-137b6a7ed2da802ca989d3035b61dfd8?pvs=4',
   },
   {
-    image: 'aplusx.webp',
-    link: 'https://www.notion.so/kwoneunjee/121b6a7ed2da8067a5cadff36d7d1f79?pvs=4#127b6a7ed2da80269e8fe1b7e49b217c',
-  }
+    image: 'pulsar.webp',
+    link: 'https://kwoneunjee.notion.site/Pulsar-137b6a7ed2da80ddb9d4fd913ec07c18?pvs=4',
+  },
+  {
+    image: 'samsung.webp',
+    link: 'https://kwoneunjee.notion.site/Samsung-UK-QA-137b6a7ed2da80358edcc5720084dcb2?pvs=4',
+  },
+  {
+    image: 'darae.webp',
+    link: 'https://kwoneunjee.notion.site/DARAE-137b6a7ed2da80eeafa4fe32dbb84f2d?pvs=4',
+  },
+  {
+    image: 'eunjeerun.webp',
+    link: 'https://kwoneunjee.notion.site/Eunjee-Run-6315c3cd536749cbaf6cc8d09930ecb5?pvs=4',
+  },
+  {
+    image: 'simple-note.webp',
+    link: 'https://kwoneunjee.notion.site/Web-Speech-Todo-List-4a1b7ec063b94cdd9e880d62f92bcde3?pvs=4',
+  },
+  {
+    image: 'vscode.webp',
+    link: 'https://kwoneunjee.notion.site/VS-code-style-e14b50020f704031ac39beeac71ee178?pvs=4',
+  },
 ];
 
 export const textSlideInfo = [
   {
-    title: "Web Speech Todo App",
+    title: "Wepublic",
     contents: [
       {
         subTitle: "설명",
-        description: "Web Speech Todo App은 음성 인식 기능을 갖춘 할 일 관리 애플리케이션입니다. Web Speetch를 활용해 음성으로 검색 기능과 여러 상태관리 라이브러리를 활용해 각각 LocalStorage에 저장되도록 기능 구현했습니다."
+        description: "블록체인 기반 소셜 플랫폼 개편, 사전예약 페이지 개발"
       },
       {
-        subTitle: "프로젝트 기술서 링크",
-        description: "https://kwoneunjee.notion.site/Toy-project-Web-speech-Todo-App-128b6a7ed2da80059bc6e644873d29c6"
+        subTitle: "프로젝트 상세 링크",
+        description: "https://www.notion.so/kwoneunjee/121b6a7ed2da8067a5cadff36d7d1f79?pvs=4#127b6a7ed2da80399aa2e72003f633e9"
       },
       {
-        subTitle: "화면 링크",
+        subTitle: "페이지 링크",
+        description: "https://www.wepublic.com/"
+      },
+      {
+        subTitle: "기술",
+        description: "NextJS, React Query, Typescript, SCSS, Storybook, Bitbucket, Jira"
+      }
+    ]
+  },
+  {
+    title: "Nile",
+    contents: [
+      {
+        subTitle: "설명",
+        description: "NFT 거래소 Nile 홈페이지 오픈, 개편, CMS 작업, 컴포넌트 작업, 어드민 페이지 작업"
+      },
+      {
+        subTitle: "프로젝트 상세 링크",
+        description: "https://kwoneunjee.notion.site/NFT-Nile-137b6a7ed2da80cf8b5eec7764a0777b?pvs=4"
+      },
+      {
+        subTitle: "페이지 링크",
+        description: "https://nile.io/"
+      },
+      {
+        subTitle: "기술",
+        description: "NextJS, Zustand, Typescript, Turborepo, Bitbucket, Jira"
+      }
+    ]
+  },
+  {
+    title: "Xdraco",
+    contents: [
+      {
+        subTitle: "설명",
+        description: "Lighthouse를 활용한 최적화, 게임 NFT 합성 페이지 개발"
+      },
+      {
+        subTitle: "프로젝트 상세 링크",
+        description: "https://kwoneunjee.notion.site/NFT-Xdraco-NFT-137b6a7ed2da802ca989d3035b61dfd8?pvs=4"
+      },
+      {
+        subTitle: "페이지 링크",
+        description: "https://www.xdraco.com/"
+      },
+      {
+        subTitle: "기술",
+        description: "Vue, NuxtJS, Pinia, Vuetify"
+      }
+    ]
+  },
+  {
+    title: "Pulsar",
+    contents: [
+      {
+        subTitle: "설명",
+        description: "이커머스 페이지 단독 개발, 서포트 페이지 단독 개발"
+      },
+      {
+        subTitle: "프로젝트 상세 링크",
+        description: "https://kwoneunjee.notion.site/Pulsar-137b6a7ed2da80ddb9d4fd913ec07c18?pvs=4"
+      },
+      {
+        subTitle: "페이지 링크",
+        description: "https://www.pulsar.gg/"
+      },
+      {
+        subTitle: "기술",
+        description: "Liquid, Javascript, SCSS"
+      }
+    ]
+  },
+  {
+    title: "Samsung UK 웹접근성 QA",
+    contents: [
+      {
+        subTitle: "설명",
+        description: "Samsung UK 사이트 웹 접근성 QA 업무"
+      },
+      {
+        subTitle: "프로젝트 상세 링크",
+        description: "https://kwoneunjee.notion.site/Samsung-UK-QA-137b6a7ed2da80358edcc5720084dcb2?pvs=4"
+      },
+      {
+        subTitle: "페이지 링크",
+        description: "https://www.samsung.com/uk"
+      }
+    ]
+  },
+  {
+    title: "DARAE",
+    contents: [
+      {
+        subTitle: "설명",
+        description: "DARAE 페이지 단독 퍼블리싱, 인터렉션, 반응형 작업"
+      },
+      {
+        subTitle: "프로젝트 상세 링크",
+        description: "https://kwoneunjee.notion.site/DARAE-137b6a7ed2da80eeafa4fe32dbb84f2d?pvs=4"
+      },
+      {
+        subTitle: "페이지 링크",
+        description: "https://daraebiz.com/"
+      },
+      {
+        subTitle: "기술",
+        description: "JQuery, Javascript, HTML, CSS"
+      }
+    ]
+  },
+  {
+    title: "Eunjee Run",
+    contents: [
+      {
+        subTitle: "설명",
+        description: "달리는 은지 단독 디자인, 기획, 개발"
+      },
+      {
+        subTitle: "프로젝트 상세 링크",
+        description: "https://kwoneunjee.notion.site/Eunjee-Run-6315c3cd536749cbaf6cc8d09930ecb5?pvs=4"
+      },
+      {
+        subTitle: "페이지 링크",
+        description: "https://eunjeerun.vercel.app/"
+      },
+      {
+        subTitle: "Github 링크",
+        description: "https://github.com/wwowww/eunjeerun"
+      },
+      {
+        subTitle: "기술",
+        description: "React, Typescript, Vite, SCSS"
+      }
+    ]
+  },
+  {
+    title: "Web Speech Todo List",
+    contents: [
+      {
+        subTitle: "설명",
+        description: "웹 스피치 기능이 있는 Todo List, 다양한 상태라이브러리를 활용해 각각 LocalStorage에 저장했습니다."
+      },
+      {
+        subTitle: "프로젝트 상세 링크",
+        description: "https://kwoneunjee.notion.site/Web-Speech-Todo-List-4a1b7ec063b94cdd9e880d62f92bcde3?pvs=4"
+      },
+      {
+        subTitle: "페이지 링크",
         description: "https://todo-app-chi-beryl-53.vercel.app/"
       },
       {
-        subTitle: "기간",
-        description: "5일 소요"
-      },
-      {
-        subTitle: "기여도",
-        description: "100%"
-      },
-      {
-        subTitle: "코드 링크",
+        subTitle: "Github 링크",
         description: "https://github.com/wwowww/projects/tree/main/apps/todo-app"
       },
       {
         subTitle: "기술",
-        description: "Typescript, React, Context API, Jotai, Recoil, Redux, Zustand"
+        description: "React, Typescript, Context API, Jotai, Recoil, Redux, Zustand"
       }
     ]
   },
   {
-    title: "VS Code Style Portfolio",
+    title: "VS code style 디자인 포트폴리오",
     contents: [
       {
         subTitle: "설명",
-        description: "편집기인 VS Code 스타일의 포트폴리오 웹사이트입니다."
+        description: "편집기인 VS code 스타일의 포트폴리오"
       },
       {
-        subTitle: "화면 링크",
+        subTitle: "프로젝트 상세 링크",
+        description: "https://kwoneunjee.notion.site/VS-code-style-e14b50020f704031ac39beeac71ee178?pvs=4"
+      },
+      {
+        subTitle: "페이지 링크",
         description: "https://portfolio-vscode-nu.vercel.app/"
       },
       {
-        subTitle: "기간",
-        description: "7일 소요"
-      },
-      {
-        subTitle: "기여도",
-        description: "100%"
-      },
-      {
-        subTitle: "코드 링크",
+        subTitle: "Github 링크",
         description: "https://github.com/wwowww/portfolio-vscode"
       },
       {
         subTitle: "기술",
-        description: "Typescript, React, React Query"
-      }
-    ]
-  },
-  {
-    title: "Wemade 경력기술서",
-    contents: [
-      {
-        subTitle: "설명",
-        description: "Wemade에 재직하며 진행한 프로젝트 기반 경력 기술서입니다."
-      },
-      {
-        subTitle: "링크",
-        description: "https://www.notion.so/kwoneunjee/121b6a7ed2da8067a5cadff36d7d1f79?pvs=4#127b6a7ed2da80399aa2e72003f633e9"
-      }
-    ]
-  },
-  {
-    title: "ApulsX 경력기술서",
-    contents: [
-      {
-        subTitle: "설명",
-        description: "ApulsX에 재직하며 진행한 프로젝트 기반 경력 기술서입니다."
-      },
-      {
-        subTitle: "링크",
-        description: "https://www.notion.so/kwoneunjee/121b6a7ed2da8067a5cadff36d7d1f79?pvs=4#127b6a7ed2da80269e8fe1b7e49b217c"
+        description: "React, Typescript, React Query, SCSS"
       }
     ]
   }
