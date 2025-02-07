39b1b3b6d850f2af2afb97bb344c6b7df0315e6b
wwowww <105021733+wwowww@users.noreply.github.com>
2025-02-08 00:58:30 +0900

    fix. gzip 압축 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import gzipPlugin from 'rollup-plugin-gzip'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,10 @@ export default defineConfig({
         api: 'modern-compiler' // or "modern"
       }
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+      plugins: [gzipPlugin()],
+    },
+  },
 })
