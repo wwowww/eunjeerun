387485ef384611e90e09ebddc2edda673ac0e3b7
wwowww <105021733+wwowww@users.noreply.github.com>
2025-02-09 17:47:46 +0900

    remove. 이미지 압축 제거

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import gzipPlugin from 'rollup-plugin-gzip'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
-    ViteImageOptimizer({
-      includePublic: true,
-      exclude: ['**/images/run/eunjee-run.webp'],
-      webp: { quality: 80 },
-    }),
   ],
   css: {
     preprocessorOptions: {
