dfa45ef520bb6cdc43a1742585c392b4b6953237
wwowww <105021733+wwowww@users.noreply.github.com>
2025-02-09 16:58:22 +0900

    chore. 이미지 크기를 줄여 로딩 속도를 개선(ViteImageOptimizer 설정)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import gzipPlugin from 'rollup-plugin-gzip'
-import tsconfigPaths from "vite-tsconfig-paths"
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    ViteImageOptimizer({
+      webp: { quality: 80 },
+    }),
+  ],
   css: {
     preprocessorOptions: {
       scss: {
